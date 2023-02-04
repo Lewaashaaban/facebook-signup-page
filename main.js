3d669cabb8b60314089d5cc8e9dc7b05f54f1132
MrPancakes39 <salmannhassan39@gmail.com>
2023-02-04 20:05:35 +0200

    When adding select current date

--- main.js
+++ main.js
@@ -17,9 +17,15 @@ function createElt(htmlString) {
  * Fills day, month, and year select HTMLElements.
  */
 function fillDates() {
+    const current_date = new Date();
+    const current_day = current_date.getDate();
+    const current_month = current_date.getMonth();
+    const current_year = current_date.getFullYear();
+
     dayElt.innerHTML = "";
     for (let i = 1; i <= 31; i++) {
         const option = createElt(`<option value="${i}">${i}</option>`);
+        if (i === current_day) option.selected = true;
         dayElt.appendChild(option);
     }
 
@@ -28,13 +34,14 @@ function fillDates() {
     const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
     months.forEach((month, i) => {
         const option = createElt(`<option value="${i + 1}">${month}</option>`);
+        if (i + 1 === current_month) option.selected = true;
         monthElt.appendChild(option);
     });
 
     yearElt.innerHTML = "";
-    const current_year = new Date().getFullYear();
     for (let i = 1970; i <= current_year; i++) {
         const option = createElt(`<option value="${i}">${i}</option>`);
+        if (i === current_year) option.selected = true;
         yearElt.appendChild(option);
     }
 }
